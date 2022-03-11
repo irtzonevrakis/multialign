7b58c9c189fe048d508b4387edc8df9b031fb781
Ioannis-Rafail Tzonevrakis <bio3103@edu.biology.uoc.gr>
2022-03-11 17:54:32 +0200

    Initial public commit.

---
+++ README.md
@@ -0,0 +1,48 @@
+# multialign.py: A wrapper script for the MDAnalysis aligner that supports multiprocessing.
+
+# Requirements
+
+* python >= 3.6
+* [MDAnalysis](https://www.mdanalysis.org/)
+* [numpy](https://numpy.org/)
+* [pymp](https://github.com/classner/pymp)
+
+Install everything with:
+
+```
+pip install MDAnalysis pymp-pypi numpy
+```
+
+# Usage
+
+```
+find /path/to/decoys/ -name '*.pdb' > pdb.lst
+MKL_NUM_THREADS=1 /path/to/multialign.py --pdblist pdb.lst --outdir /path/to/aligned/decoys
+```
+
+Replace MKL_NUM_THREADS with OPENBLAS_NUM_THREADS if your numpy uses openblas instead. In any case, the script should emit a warning if the environment has not been set up correctly.
+
+The number of threads can be customized using the `--num_threads` parameter. The default, `-1`, will use all available threads, as determined by calling `os.sched_getaffinity(0)`.
+
+# Copyright/License
+
+```
+    Copyright (C) 2022, The Institute of Computer Science-FORTH /
+                        Ioannis-Rafail Tzonevrakis
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+```
+
+For more details, see the [LICENSE](LICENSE) file in this repository. For the licenses of the libraries used by this script, see the [LICENSE_COMPONENTS](LICENSE_COPONENTS) file.
---
+++ multialign.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python3
+''' multialign.py: A wrapper script for the MDAnalysis aligner that supports 
+                   multiprocessing.
+
+    Copyright (C) 2022, The Institute of Computer Science-FORTH /
+                        Ioannis-Rafail Tzonevrakis
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+'''
+
+from __future__ import print_function
+import argparse, os, pymp
+import numpy as np
+
+from MDAnalysis import Universe
+from MDAnalysis.analysis import align
+from MDAnalysis.analysis.rms import rmsd
+
+get_rmsd = lambda mobile, ref: rmsd(mobile.select_atoms('name CA').positions, 
+                                    ref.select_atoms('name CA').positions)
+
+def align_parallel(fnames, ref, num_threads):
+    max_cpus = len(os.sched_getaffinity(0))
+    has_mkl = 'mkl' in ''.join(np.__config__.get_info('blas_mkl_info')\
+                               ['libraries'])
+    var_name = 'OPENBLAS_NUM_THREADS'
+    if has_mkl:
+        var_name = 'MKL_NUM_THREADS'
+    suboptimal_np_threads = False
+    if var_name in os.environ.keys():
+        if os.environ[var_name] != '1':
+            suboptimal_np_threads = True
+    else:
+        suboptimal_np_threads = True
+    if suboptimal_np_threads:
+        print(f'WARN: Running with {var_name} != 1. Sub-optimal performance '+
+              f'may ensue. We recommend re-running the script with {var_name}'+
+              ' set to 1.')
+    if num_threads > max_cpus:
+        print(f'WARN: Number of threads {num_threads} larger than maximum '+
+              f'alotted threads {max_cpus}. Setting to {max_cpus} instead.')
+        num_threads = max_cpus
+    if num_threads == -1:
+        num_threads = max_cpus
+    print(f'Running with {num_threads} threads.')
+    fnames = np.array(fnames)
+    shmem = pymp.shared.array((len(fnames),), dtype=fnames.dtype)
+    shmem = fnames
+    with pymp.Parallel(num_threads) as p:
+        for i in p.range(1, len(fnames)):
+            mobile = Universe(shmem[i])
+            print(f'Decoy {fnames[i]}, RMSD to ref before alignment: '+
+                  f'{get_rmsd(mobile, ref)}')
+            align.AlignTraj(mobile, ref, in_memory=True).run()
+            mobile.select_atoms('all').write(os.path.join(outdir, 
+                                             os.path.basename(shmem[i])))
+            print(f'Decoy {fnames[i]}, RMSD to ref after alignment: '+
+                  f'{get_rmsd(mobile, ref)}')
+
+
+if __name__ == '__main__':
+    parser=argparse.ArgumentParser()
+    parser.add_argument('--pdblist', type=str, required=True,
+                        help='Path to list of PDBs. The first PDB is used as '+
+                             'reference structure.')
+    parser.add_argument('--outdir', type=str, required=True,
+                        help='Path to directory to store aligned PDBs in.')
+    parser.add_argument('--num_threads', type=int, required=False, default=-1,
+                        help='Number of threads to use (defaults to maximum '+
+                              'available).')
+    args = parser.parse_args()
+    pdblist = args.pdblist
+    outdir = args.outdir
+    num_threads = args.num_threads
+    if not os.path.exists(outdir):
+        os.mkdir(outdir)
+
+    fnames = []
+    with open(pdblist) as fp:
+        for fname in fp:
+            fnames.append(fname.strip())
+
+    print(f'Using {fnames[0]} as reference')
+    ref = Universe(fnames[0])
+    ref.select_atoms('all').write(os.path.join(outdir, 
+                                  os.path.basename(fnames[0])))
+
+    align_parallel(fnames, ref, num_threads)
